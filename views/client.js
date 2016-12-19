f2c4ee17a19620d04a154cad6fed1a2a8b858ca0
Zachary Sang <zacharysang@gmail.com>
2016-12-18 20:52:20 -0500

    moved javascript into separate client.js file

---
+++ views/client.js
@@ -0,0 +1,34 @@
+//menu button toggling
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+
+//facebook sdk
+	console.log("initializing fb sdk");
+	
+	//this actually loads the SDK
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=430107553824095";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	console.log("fb sdk initialized");
+	
+	//this function is called after the SDK is loaded
+	window.fbAsyncInit = function() {
+		//registers with smashthehouse facebook app
+		FB.init({
+			appId      : '430107553824095',
+			xfbml      : false,
+			version    : 'v2.8',
+			status	   : true
+		});
+		
+	};
+
+//login code
+
