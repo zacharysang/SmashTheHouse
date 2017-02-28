eb792a2966a11023327404368d962a47c50c46bf
Zachary Sang <zacharysang@gmail.com>
2017-02-27 21:57:21 -0500

    Worked on status page

--- routes/status.js
+++ routes/status.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
+var dns = require('dns');
 
 /* GET news page. */
 router.get('/', function(req, res,next) {
-  res.render('status', { title: 'SmashTheHouse' });
+  res.render('status', { protocol: 'http' , uptime: os.uptime(), ipaddr: String(os.networkInterfaces()), totalMem: os.totalmem()/1000000000, freeMem: os.freemem()/1000000000});
 });
 
 module.exports = router;
