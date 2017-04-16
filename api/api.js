7a3ec98ec9ac3331ad747b9e3eb32ae1745388d6
Zachary Sang <zacharysang@gmail.com>
2017-04-16 00:05:45 -0400

    fixed movie loading/rendering (still needs db hookup)

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 router.get('*',function(req,res){
-  res.movies = [{
+  var movies = [{
         "title": "f8"
         ,"description": "one last job"
         ,"releaseDate": "12/12/12"
@@ -17,7 +17,7 @@ router.get('*',function(req,res){
         ,"description": "somethings never change, but sometimes they do.."
         ,"releaseDate": "12/12/16"
     }];
-  res.send();
+  res.send(movies);
 });
 
 module.exports = router;
--- public/js/movies.js
+++ public/js/movies.js
@@ -1,31 +1,35 @@
-if(!availableMovies) var availableMovies = [];
-
+var availableMovies = [];
 fetch('/api/movies')
 .then(function(res){
     console.log('fetch returned');
     // get the list out of the response
-    return res.blob();
+    return res.json();
 }).then(function(res){
-    
-    if(res.movies){
-        availableMovies = res.movies;
+    var moviesList;
+    if(res !== undefined && res.length > 0){
+        console.log('loading in movies');
+        moviesList = res;
     }else{
-        availableMovies = [];
+        console.log('no movies found attached to request. passing empty arr');
+        moviesList = [];
     }
-}).then(function(){
-    renderMovies();
+    
+    console.log(`json to be returned: ${JSON.stringify(moviesList)}`);
+    return moviesList;
+}).then(function(moviesL){
+    renderMovies(moviesL);
 });
 
-function renderMovies(addMovies = availableMovies){
-    console.log('rendering movies..');
+function renderMovies(movies){
+    console.log(`movies list from render: ${JSON.stringify(movies)}`)
     var listContainer = $('ul.moviesList')
     var currMov;
-    addMovies.forEach(function(mov){
+    movies.forEach(function(mov){
         currMov = $(`<li>${mov.title} (${mov.releaseDate})</li>`,{
             "class":"movieItem"
         })
     .appendTo(listContainer);
-
+        
     });
     
 }
