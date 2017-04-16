10294f2cb9b3faea90c3c3ffcf02d83e7089e061
Zachary Sang <zacharysang@gmail.com>
2017-04-16 16:04:53 -0400

    Added basic recycling stuff

--- public/js/recycle.js
+++ public/js/recycle.js
@@ -4,6 +4,15 @@ var weeksSince = moment().diff(knownDate,'weeks',true);
 
 var isRecyclingWeek = weeksSince % 2 > 1;
 
-if(isRecyclingWeek){
-    $('html').css('background-color','green');
-}
+$(document).ready(function(){
+    var recycleEl = document.querySelectorAll('.recycleDays')[0];
+
+    if(isRecyclingWeek){
+        recycleEl.innerText = "None! Recycling came today!";
+    }else{
+        recycleEl.innerText = moment().diff(knownDate,'days');
+    }
+});
+
+
+
