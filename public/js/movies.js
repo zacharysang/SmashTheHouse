7ac1c04784435a582fb45a6d944fed869beb41be
Zachary Sang <zacharysang@gmail.com>
2017-04-16 15:18:26 -0400

    Reworked movie styling

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,35 +1,45 @@
-var availableMovies = [];
 fetch('/api/movies')
 .then(function(res){
-    console.log('fetch returned');
     // get the list out of the response
     return res.json();
 }).then(function(res){
     var moviesList;
     if(res !== undefined && res.length > 0){
-        console.log('loading in movies');
         moviesList = res;
     }else{
-        console.log('no movies found attached to request. passing empty arr');
         moviesList = [];
     }
     
-    console.log(`json to be returned: ${JSON.stringify(moviesList)}`);
     return moviesList;
 }).then(function(moviesL){
     renderMovies(moviesL);
 });
 
 function renderMovies(movies){
-    console.log(`movies list from render: ${JSON.stringify(movies)}`)
-    var listContainer = $('ul.moviesList')
+    var listContainer = document.querySelector('ul.moviesList');
     var currMov;
     movies.forEach(function(mov){
-        currMov = $(`<li>${mov.title} (${mov.releaseDate})</li>`,{
-            "class":"movieItem"
-        })
-    .appendTo(listContainer);
+        currMov = renderMovie(mov);
+        listContainer.appendChild(currMov);
         
-    });
-    
+    });    
 }
+
+function renderMovie(mov){
+    var movieLi = document.createElement('li');
+    movieLi.setAttribute('class','movieItem');
+
+    var headerDiv = document.createElement('div');
+    headerDiv.setAttribute('class','movieHeader');
+    headerDiv.innerText = `${mov.title} (${mov.releaseDate})`;
+
+    var descDiv = document.createElement('div');
+    descDiv.setAttribute('class','movieDesc');
+    descDiv.innerText = mov.description;
+
+    movieLi.appendChild(headerDiv);
+    movieLi.appendChild(descDiv);
+
+    return movieLi;
+}
+
