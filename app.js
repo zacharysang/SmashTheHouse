a9f692b580b9e6c1b95a851b8e36454a52eafcd5
Zachary Sang <zacharysang@gmail.com>
2017-04-19 09:59:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var env = require('./env');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var api = require('./api/api');
+var env = require('./env')
+    ,express = require('express')
+    ,path = require('path')
+    ,favicon = require('serve-favicon')
+    ,logger = require('morgan')
+    ,cookieParser = require('cookie-parser')
+    ,bodyParser = require('body-parser')
+    ,mongoose = require('mongoose')
+    ,api = require('./api/api');
 
 mongoose.connect(env.MONGO_URL,function(err){
   if(err){
