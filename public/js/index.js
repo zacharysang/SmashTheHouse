5b732960507b743e8af77b84b8ea8b7c2af1a30d
Zachary Sang <zacharysang@gmail.com>
2017-04-28 21:41:21 -0400

    Added support for battery status

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,40 @@
 $(document).ready(function(){
 var tileEls = $('.noticeTile');
 
+
+// try to get the batter status
+var battStr = '';
+if(navigator.battery){
+    //if navigator has battery object already (people shouldn't do this. it's old)
+    readBattery(navigator.battery);
+}else if(navigator.getBattery){
+    //if navigator holds promise
+    navigator.getBattery().then(readBattery);
+}else{
+    //not supported
+}
+
+//afix the notice display functionality to all noticeTiles
 tileEls.each(function(index){
     var tileName = $(this).attr('title');
     $(this).hover(
         function(ev){
-            console.log('in');
             $(`.noticeMessage[title="${$(ev.target).attr('title')}"]`).show();
         },
         function(ev){
-            console.log('leaving');
             $(`.noticeMessage[title='${$(ev.target).attr('title')}']`).hide();
         }
     );
 });
+
+
 });
+
+function readBattery(battery){
+
+    //read batter status into the notice message text
+    $(`.noticeMessage[title='Battery']`).text(`${battery.level*100}%`);
+    
+
+}
 
