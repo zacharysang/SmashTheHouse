8e924156a0a100cc310fd484454a2ccf8324add1
Zachary Sang <zacharysang@gmail.com>
2017-04-30 17:00:38 -0400

    Moved battery functionality into its own file

---
+++ public/js/battery.js
@@ -0,0 +1,28 @@
+// try to get the batter status
+if(navigator.getBattery){
+    //if navigator holds promise
+    navigator.getBattery().then(readBattery);
+}if(navigator.battery){
+    //if navigator has battery object already (people shouldn't do this. it's old)
+    readBattery(navigator.battery);
+}else{
+    //not supported
+}
+
+
+function readBattery(battery){
+
+    //read batter status into the notice message text
+    $(`[title="Battery"] > .noticeMessage`).text(`${battery.level*100}%`);
+
+    //read into the tile color
+    if(battery.level > 0.8){
+        $(`[title="Battery"] > .noticeTile`).css('background-color',`#00E500`);
+    } else if(battery.level > 0.25){
+        $(`[title="Battery"] > .noticeTile`).css('background-color',`yellow`);
+    } else {
+        $(`[title="Battery"] > .noticeTile`).css('background-color',`red`);
+    }
+    
+
+}
--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,6 @@ $(document).ready(function(){
 var tileEls = $('.noticeTile');
 
 
-// try to get the batter status
-var battStr = '';
-if(navigator.battery){
-    //if navigator has battery object already (people shouldn't do this. it's old)
-    readBattery(navigator.battery);
-}else if(navigator.getBattery){
-    //if navigator holds promise
-    navigator.getBattery().then(readBattery);
-}else{
-    //not supported
-}
-
 //afix the notice display functionality to all noticeTiles
 tileEls.each(function(index){
     var tileName = $(this).attr('title');
@@ -29,21 +17,3 @@ tileEls.each(function(index){
 
 
 });
-
-function readBattery(battery){
-
-    //read batter status into the notice message text
-    $(`[title="Battery"] > .noticeMessage`).text(`${battery.level*100}%`);
-
-    //read into the tile color
-    if(battery.level > 0.8){
-        $(`[title="Battery"] > .noticeTile`).css('background-color',`#00E500`);
-    } else if(battery.level > 0.25){
-        $(`[title="Battery"] > .noticeTile`).css('background-color',`yellow`);
-    } else {
-        $(`[title="Battery"] > .noticeTile`).css('background-color',`red`);
-    }
-    
-
-}
-
