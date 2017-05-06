d94bb40cd020ac723f729898b6d60df659f849d6
Zachary Sang <zacharysang@gmail.com>
2017-05-06 15:16:33 -0400

    Improved filling functionality (added drag filling)

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-    $('.guideBox').click(function(){
-        if($(this).hasClass('filled')){
-            $(this).removeClass('filled');
-        } else {
-            $(this).addClass('filled');
-        }
+
+    $('.workingArea').on('mousedown',function(){
+        //when mouse is down, fill everything moused-enter
+        $('.guideBox').on('mouseenter',function(){
+            toggle(this);
+        });
+
+    });
+
+    $('.workingArea').on('mouseup',function(){
+        //when mouse is lifted, stop filling everything
+        $('.guideBox').off('mouseenter');
     });
 });
+
+function toggle(JEl){
+        if($(JEl).hasClass('filled')){
+            $(JEl).removeClass('filled');
+        } else {
+            $(JEl).addClass('filled');
+        }
+
+}
