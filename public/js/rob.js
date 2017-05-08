a573c8a627c284c4a31c0833136adcecf82b05f6
Zachary Sang <zacharysang@gmail.com>
2017-05-08 14:00:43 -0400

    Added rob special day 7777

--- public/js/rob.js
+++ public/js/rob.js
@@ -2,7 +2,8 @@ function printRob(){
 
 //date of loss
 dol = moment("1996-05-11")
-days = moment.duration(moment().diff(dol)).asDays();
+days = Math.floor(moment.duration(moment().diff(dol)).asDays());
+var emoji = days == 7777 ? `\uD83C\uDF40` : '';
+$(`[title="Rob"] > .noticeMessage`).text(emoji + `Rob has been gone for ${days} days` + emoji);
 
-$(`[title="Rob"] > .noticeMessage`).text(`Rob has been gone for ${Math.floor(days)} days`);
 }
