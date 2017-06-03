ee4bebc291134da3f4c90ffab0a269b50ef3639c
Zachary Sang <zacharysang@gmail.com>
2017-06-02 20:43:38 -0400

    polished bars

--- public/js/scratch.js
+++ public/js/scratch.js
@@ -4,7 +4,7 @@ bars = $('.bar');
 offsets = [];
 
 for(var i = 0; i < bars.length; i++){
-    var offsetStr = `${i*100}`;
+    var offsetStr = `${(i+1)*100}`;
     offsets.push(offsetStr);
     bars.css('left',offsetStr);
 }
@@ -13,17 +13,17 @@ $('#start').on('click',rotateBars);
 
 
 
-
-
 });
 
 function rotateBars(ev){
     for(i = 0; i < bars.length; i++){
-        $(bars[i]).animate({left:offsets[i]},500);
+        $(bars[i]).animate({left:offsets[i]},200);
 
+    }
+
+    for(i = 0; i < bars.length; i++){
         //rotate offsets
-        bars[i] = bars[(i+1)%5];
-
+        bars[i] = bars[(i+1)%6];
     }
 
 
